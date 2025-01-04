a8daddd8d5a69da28ede8dc8ed0dd0dcde0c92c9
Svyatoslav Shypka <svyat200909@gmail.com>
2025-01-04 12:01:41 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  List<String> groceries = ['Apples', 'Oranges', 'Bananas'];
+
+  List<double> prices = [1.03, 2.08, 1.73];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              const Expanded(child: TextField()),
+              ElevatedButton(
+                child: const Icon(Icons.add),
+                onPressed: () {
+                  groceries.add('test');
+                  print(groceries);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: groceries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: ListTile(
+                title: Text(groceries[index]),
+                // trailing: const Text('\$0'),
+                trailing: Text('\$${prices[index]}'),
+              ),
+            );
+          },
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ));
+  }
+}
