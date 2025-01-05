a834df72d2e52d9d49e7fbb988ea006e3ffbf8db
Svyatoslav Shypka <svyat200909@gmail.com>
2025-01-05 09:04:45 +0100

    await

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -19,6 +19,7 @@ void main() {
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
+
     await tester.pump();
 
     // Verify that our counter has incremented.
