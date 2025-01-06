b038396315b39b8b638cc9f2598bff41ac8f9320
Svyatoslav Shypka <svyat200909@gmail.com>
2025-01-06 09:07:44 +0100

    episode5.Ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  List<String> groceries = ['Apples', 'Oranges', 'Bananas'];
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  List<double> prices = [1.03, 5.08, 1.73];
+class _MyAppState extends State<MyApp> {
+  String buttonLabel = 'Before Pressed';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              const Expanded(child: TextField()),
-              ElevatedButton(
-                child: const Icon(Icons.add),
-                onPressed: () {
-                  groceries.add('test');
-                  print(groceries);
-                },
-              ),
-            ],
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                buttonLabel = 'After Pressed';
+              });
+              print(buttonLabel);
+            },
+            child: Text(buttonLabel),
           ),
         ),
-        body: ListView.builder(
-          itemCount: groceries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                title: Text(groceries[index]),
-                // trailing: const Text('\$0'),
-                trailing: Text('\$${prices[index]}'),
-              ),
-            );
-          },
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
-        ));
+      ),
+    );
   }
 }
